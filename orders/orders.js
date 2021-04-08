40e1030f5671640f8d374e7e047faa6a30563f03
retgits <lstigter@gmail.com>
2021-04-08 13:44:15 -0700

    feat: update orders to SDK 0.7.0-beta.1

--- orders/orders.js
+++ orders/orders.js
@@ -24,15 +24,14 @@
   * Create a new EventSourced entity with parameters
   * * An array of protobuf files where the entity can find message definitions
   * * The fully qualified name of the service that provides this entities interface
+  * * The entity type name for all event source entities of this type. This will be prefixed
+  *   onto the entityId when storing the events for this entity.
   */
 const entity = new EventSourcedEntity(
-    ['orders.proto'],
-    'ecommerce.Orders',
-    {
-        // A persistence id for all value entities of this type. This will be prefixed onto 
-        // the entityId when storing the state for this entity.
-        entityType: 'orders',
-        
+    ['orders.proto', 'domain.proto'],
+    'ecommerce.OrderService',
+    'orders',
+    {        
         // A snapshot will be persisted every time this many events are emitted.
         snapshotEvery: 100,
         
@@ -50,14 +49,35 @@ const entity = new EventSourcedEntity(
 );
 
 /**
+ * The events and state that are stored in Akka Serverless are in Protobuf format. To make it
+ * easier to work with, you can load the protobuf types (as happens in the below code). The
+ * Protobuf types are needed so that Akka Serverless knowns how to serialize these objects when 
+ * they are persisted.
+ */
+ const pkg = 'ecommerce.persistence.';
+ const OrderAdded = entity.lookupType(pkg + 'OrderAdded');
+ const Orders = entity.lookupType(pkg + 'Orders');
+
+/**
  * Set a callback to create the initial state. This is what is created if there is no snapshot
  * to load, in other words when the entity is created and nothing else exists for it yet.
  *
  * The userID parameter can be ignored, it's the id of the entity which is automatically 
  * associated with all events and state for this entity.
  */
-entity.setInitial(userID => ({}));
+entity.setInitial(userID => Orders.create({
+    userID: '',
+    orders: []
+}));
 
+/**
+ * Set a callback to create the behavior given the current state. Since there is no state
+ * machine like behavior transitions for this entity, we just return one behavior, but
+ * you could return multiple different behaviors depending on the state.
+ *
+ * This callback will be invoked after each time that an event is handled to get the current
+ * behavior for the current state.
+ */
 entity.setBehavior(orders => {
     return {
         commandHandlers: {
@@ -83,14 +103,19 @@ entity.setBehavior(orders => {
  * 
  * @param {*} newOrder the order to be added
  * @param {*} orderHistory an empty placeholder
- * @param {*} ctx the Cloudstate context
+ * @param {*} ctx the Akka Serverless context
+ * @returns
  */
 function addOrder(newOrder, orderHistory, ctx) {
     console.log(`Adding order ${newOrder.orderID} to the history of user ${newOrder.userID}`)
-    ctx.emit({
-        type: 'OrderAdded',
-        order: newOrder
-    })
+
+    const no = OrderAdded.create({
+        userID: newOrder.userID,
+        orderID: newOrder.orderID,
+        items: newOrder.items
+    });
+
+    ctx.emit(no)
     return newOrder
 }
 
@@ -100,7 +125,8 @@ function addOrder(newOrder, orderHistory, ctx) {
  * 
  * @param {*} request the user to get the order history for and the orderID to find
  * @param {*} orderHistory the entire state for the user (the entity) from which to filter the order
- * @param {*} ctx the Cloudstate context
+ * @param {*} ctx the Akka Serverless context
+ * @returns
  */
 function getOrderDetails(request, orderHistory, ctx) {
     let found = orderHistory.orders.find(order => {
@@ -122,6 +148,7 @@ function getOrderDetails(request, orderHistory, ctx) {
  * 
  * @param {*} request the user to get the order history for
  * @param {*} orderHistory the entire state for the user (the entity)
+ * @returns
  */
 function getOrderHistory(request, orderHistory) {
     console.log(`Getting all orders for ${orderHistory.userID}`)
@@ -135,14 +162,14 @@ function getOrderHistory(request, orderHistory) {
 **/
 
 function orderAdded(orderInfo, orderHistory) {
-    console.log(`Updating the order history for ${orderInfo.order.userID}`)
+    console.log(`Updating the order history for ${orderInfo.userID}`)
 
     if(orderHistory.orders) {
-        orderHistory.orders.push(orderInfo.order)
+        orderHistory.orders.push(orderInfo)
     } else {
         orderHistory = {
-            userID: orderInfo.order.userID,
-            orders: new Array(orderInfo.order)
+            userID: orderInfo.userID,
+            orders: new Array(orderInfo)
         }
     }
 
